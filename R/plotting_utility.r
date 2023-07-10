78587754883221cb35d21ddf6886cc34b3156839
Yichen Wang <wangych@bu.edu>
2023-07-10 17:32:28 -0400

    Update unique color generator

--- R/plotting_utility.r
+++ R/plotting_utility.r
@@ -307,15 +307,43 @@ png_as_grob <- function(filename) {
     grid::rasterGrob(image, interpolate = TRUE)
 }
 
+.ggsci_db_igv_default <- c(
+  "#5050FF", "#CE3D32", "#749B58", "#F0E685", "#466983", "#BA6338", "#5DB1DD",
+  "#802268", "#6BD76B", "#D595A7", "#924822", "#837B8D", "#C75127", "#D58F5C",
+  "#7A65A5", "#E4AF69", "#3B1B53", "#CDDEB7", "#612A79", "#AE1F63", "#E7C76F",
+  "#5A655E", "#CC9900", "#99CC00", "#A9A9A9", "#33CC00", "#00CC33", "#00CC99",
+  "#0099CC", "#0A47FF", "#4775FF", "#FFC20A", "#FFD147", "#990033", "#991A00",
+  "#996600", "#809900", "#339900", "#00991A", "#009966", "#008099", "#003399",
+  "#1A0099", "#660099", "#990080", "#D60047", "#FF1463", "#00D68F", "#14FFB1"
+)
+
+
+#' Generate character vector of unique color hex codes
+#' Base color palette adopts \code{ggsci:::ggsci_db$igv$default}
+#' @param n Number of unique colors to generate. When not exceeding existing
+#' number of colors, just return the first \code{n} colors, otherwise generate
+#' interpolated colors.
+uniqueColors <- function(n) {
+  if (n <= length(.ggsci_db_igv_default)) {
+    .ggsci_db_igv_default[seq_len(n)]
+  } else {
+    warning("More than ", length(.ggsci_db_igv_default), " unique colors ",
+            "requested. Interpolating preset color set.")
+    grDevices:::colorRampPalette(.ggsci_db_igv_default)(n)
+  }
+}
+
 .checkColorList <- function(object, colors.list = NULL) {
   if (is.null(colors.list)) {
     levels.use <- levels(object@clusters)
-    colors.list <- as.character(paletteer::paletteer_d("ggsci::default_igv",
-                                                       n = length(levels.use)))
+    colors.list <- uniqueColors(length(levels.use))
     names(colors.list) <- levels.use
+    return(colors.list)
   }
 
-  if (length(colors.list) != length(levels(object@clusters))) {
+  col.len <- length(unique(colors.list))
+
+  if (!identical(col.len, length(levels(object@clusters)))) {
     stop("The length of `colors.list` is not equal to the number of clusters.")
   }
 
